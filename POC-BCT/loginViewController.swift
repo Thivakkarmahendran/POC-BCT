00ee28f36b689d05ee57601b0a4a542b70b15dce
Thivakkar Mahendran <mahendrans@macbook-pro-3.local>
2018-06-21 14:19:44 -0700

    New login and sign up ui, both combined into one view

---
+++ POC-BCT/loginViewController.swift
@@ -0,0 +1,268 @@
+//
+//  loginViewController.swift
+//  POC-BCT
+//
+//  Created by Thivakkar Mahendran on 6/21/18.
+//  Copyright © 2018 Thivakkar Mahendran. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+enum AMLoginSignupViewMode {
+    case login
+    case signup
+}
+
+class loginViewController: UIViewController {
+    
+    
+    let animationDuration = 0.25
+    var mode:AMLoginSignupViewMode = .signup
+    
+    
+    //MARK: - background image constraints
+    @IBOutlet weak var backImageLeftConstraint: NSLayoutConstraint!
+    @IBOutlet weak var backImageBottomConstraint: NSLayoutConstraint!
+    
+    
+    //MARK: - login views and constrains
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var loginContentView: UIView!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginButtonVerticalCenterConstraint: NSLayoutConstraint!
+    @IBOutlet weak var loginButtonTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var loginWidthConstraint: NSLayoutConstraint!
+    
+    
+    //MARK: - signup views and constrains
+    @IBOutlet weak var signupView: UIView!
+    @IBOutlet weak var signupContentView: UIView!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var signupButtonVerticalCenterConstraint: NSLayoutConstraint!
+    @IBOutlet weak var signupButtonTopConstraint: NSLayoutConstraint!
+    
+    
+    //MARK: - logo and constrains
+    @IBOutlet weak var logoView: UIView!
+    @IBOutlet weak var logoTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var logoHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var logoBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var logoButtomInSingupConstraint: NSLayoutConstraint!
+    @IBOutlet weak var logoCenterConstraint: NSLayoutConstraint!
+    
+    
+    @IBOutlet weak var forgotPassTopConstraint: NSLayoutConstraint!
+
+    
+    
+    //MARK: - input views
+    @IBOutlet weak var loginEmailInputView: AMInputView!
+    @IBOutlet weak var loginPasswordInputView: AMInputView!
+    @IBOutlet weak var signupEmailInputView: AMInputView!
+    @IBOutlet weak var signupPasswordInputView: AMInputView!
+    @IBOutlet weak var signupPasswordConfirmInputView: AMInputView!
+    
+    
+    
+    //MARK: - controller
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // set view to login mode
+        toggleViewMode(animated: false)
+        
+        //add keyboard notification
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboarFrameChange(notification:)), name: .UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    
+    //MARK: - button actions
+    @IBAction func loginButtonTouchUpInside(_ sender: AnyObject) {
+        
+        if mode == .signup {
+            toggleViewMode(animated: true)
+            
+        }else{
+            
+            //TODO: login by this data
+            Auth.auth().signIn(withEmail: loginEmailInputView.textFieldView.text!, password: loginPasswordInputView.textFieldView.text!) { (user, error) in
+                
+                if(user == nil){ // ERROR
+                    let alert = UIAlertController(title: "Error", message: error?.localizedDescription as! String, preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+                else if let user = user { // SIGN In sucess!!
+                    UserID = user.user.uid
+                    
+                    let loginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "home") as! homeViewController
+                    self.present(loginVC, animated: true, completion: nil)
+                }
+            }
+            
+            
+        }
+    }
+    
+    @IBAction func signupButtonTouchUpInside(_ sender: AnyObject) {
+        
+        if mode == .login {
+            toggleViewMode(animated: true)
+        }else{
+            
+            //TODO: signup by this data
+            NSLog("Email:\(signupEmailInputView.textFieldView.text) Password:\(signupPasswordInputView.textFieldView.text), PasswordConfirm:\(signupPasswordConfirmInputView.textFieldView.text)")
+            
+            if((signupPasswordInputView.textFieldView.text! == signupPasswordConfirmInputView.textFieldView.text!) && (signupPasswordInputView.textFieldView.text! != "") && (signupEmailInputView.textFieldView.text! != "")){
+                
+                Auth.auth().createUser(withEmail: signupEmailInputView.textFieldView.text!, password: signupPasswordConfirmInputView.textFieldView.text!) { (authResult, error) in
+                    if(authResult != nil){
+                        ///// navigate to the home screen
+                        UserID = (authResult?.user.uid)!
+                        
+                        let loginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "home") as! homeViewController
+                        self.present(loginVC, animated: true, completion: nil)
+                        
+                    }
+                    else{
+                        let alert = UIAlertController(title: "Error", message: error?.localizedDescription as! String, preferredStyle: UIAlertControllerStyle.alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+            }
+            else{
+                let alert = UIAlertController(title: "Error", message: "Check your email or password", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+            
+            
+            
+        }
+    }
+    
+    
+    
+    //MARK: - toggle view
+    func toggleViewMode(animated:Bool){
+        
+        // toggle mode
+        mode = mode == .login ? .signup:.login
+        
+        
+        // set constraints changes
+        backImageLeftConstraint.constant = mode == .login ? 0:-self.view.frame.size.width
+        
+        
+        loginWidthConstraint.isActive = mode == .signup ? true:false
+        logoCenterConstraint.constant = (mode == .login ? -1:1) * (loginWidthConstraint.multiplier * self.view.frame.size.width)/2
+        loginButtonVerticalCenterConstraint.priority = UILayoutPriority(rawValue: UILayoutPriority.RawValue(mode == .login ? 300:900))
+        signupButtonVerticalCenterConstraint.priority = UILayoutPriority(rawValue: UILayoutPriority.RawValue(mode == .signup ? 300:900))
+        
+        
+        //animate
+        self.view.endEditing(true)
+        
+        UIView.animate(withDuration:animated ? animationDuration:0) {
+            
+            //animate constraints
+            self.view.layoutIfNeeded()
+            
+            //hide or show views
+            self.loginContentView.alpha = self.mode == .login ? 1:0
+            self.signupContentView.alpha = self.mode == .signup ? 1:0
+            
+            
+            // rotate and scale login button
+            let scaleLogin:CGFloat = self.mode == .login ? 1:0.4
+            let rotateAngleLogin:CGFloat = self.mode == .login ? 0:CGFloat(-M_PI_2)
+            
+            var transformLogin = CGAffineTransform(scaleX: scaleLogin, y: scaleLogin)
+            transformLogin = transformLogin.rotated(by: rotateAngleLogin)
+            self.loginButton.transform = transformLogin
+            
+            
+            // rotate and scale signup button
+            let scaleSignup:CGFloat = self.mode == .signup ? 1:0.4
+            let rotateAngleSignup:CGFloat = self.mode == .signup ? 0:CGFloat(-M_PI_2)
+            
+            var transformSignup = CGAffineTransform(scaleX: scaleSignup, y: scaleSignup)
+            transformSignup = transformSignup.rotated(by: rotateAngleSignup)
+            self.signupButton.transform = transformSignup
+        }
+        
+    }
+    
+    
+    //MARK: - keyboard
+    @objc func keyboarFrameChange(notification:NSNotification){
+        
+        let userInfo = notification.userInfo as! [String:AnyObject]
+        
+        // get top of keyboard in view
+        let topOfKetboard = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue .origin.y
+        
+        
+        // get animation curve for animate view like keyboard animation
+        var animationDuration:TimeInterval = 0.25
+        var animationCurve:UIViewAnimationCurve = .easeOut
+        if let animDuration = userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber {
+            animationDuration = animDuration.doubleValue
+        }
+        
+        if let animCurve = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber {
+            animationCurve =  UIViewAnimationCurve.init(rawValue: animCurve.intValue)!
+        }
+        
+        
+        // check keyboard is showing
+        let keyboardShow = topOfKetboard != self.view.frame.size.height
+        
+        
+        //hide logo in little devices
+        let hideLogo = self.view.frame.size.height < 667
+        
+        // set constraints
+        backImageBottomConstraint.constant = self.view.frame.size.height - topOfKetboard
+        
+        logoTopConstraint.constant = keyboardShow ? (hideLogo ? 0:20):50
+        logoHeightConstraint.constant = keyboardShow ? (hideLogo ? 0:40):60
+        logoBottomConstraint.constant = keyboardShow ? 20:32
+        logoButtomInSingupConstraint.constant = keyboardShow ? 20:32
+        
+        forgotPassTopConstraint.constant = keyboardShow ? 30:45
+        
+        loginButtonTopConstraint.constant = keyboardShow ? 25:30
+        signupButtonTopConstraint.constant = keyboardShow ? 23:35
+        
+        loginButton.alpha = keyboardShow ? 1:0.7
+        signupButton.alpha = keyboardShow ? 1:0.7
+        
+        
+        
+        // animate constraints changes
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDuration(animationDuration)
+        UIView.setAnimationCurve(animationCurve)
+        
+        self.view.layoutIfNeeded()
+        
+        UIView.commitAnimations()
+        
+    }
+    
+    //MARK: - hide status bar in swift3
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
+
